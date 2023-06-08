082ba6b9afa52b65913f1d4a74420110d6d93289
granth <granthbagadia2004@gmail.com>
2023-06-08 19:46:26 +0530

    all_changes

--- detect.py
+++ detect.py
@@ -28,15 +28,15 @@ def analyze(IMAGE_FILE):
             if round(conf,2) >= .75:
                 Output['detected'].append([int(round(x1,1)), int(round(y1,1)), int(round(x2,1)), int(round(y2,1))])
 
-    if per_c == 0:
+    if blur_check(IMAGE_FILE) == True:
+        Output['content'] = 'Image is Blurry'
+    elif per_c == 0:
         Output['content'] = 'No person Detected'
     elif per_c > 1:
         Output['content'] = 'More than 1 person Detected'
     elif len(Output['detected']) > 1:
         Output['content'] = 'Illegal Object Detected'
     else:
-        Output['content'] = 'None'
-
-    Output['blur'] = blur_check(IMAGE_FILE)
+        Output['content'] = False
 
     return Output
