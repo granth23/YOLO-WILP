caddc7efe791ae5c34a5cd2a9b886ccb40b876ac
granth <granthbagadia2004@gmail.com>
2023-07-30 17:56:37 +0530

    build

---
+++ test.py
@@ -0,0 +1,13 @@
+import requests
+import os.path
+
+path = 'models/yolov7.pt'
+
+check_file = os.path.isfile(path)
+
+if check_file == True:
+    pass
+else:
+    URL = "https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7.pt"
+    response = requests.get(URL)
+    open(path, "wb").write(response.content)
